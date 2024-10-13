b21281c9bded41fa4723a6b97de8bec4467d5072
Borealis <borealiscreadoresdeideas@gmail.com>
2024-10-13 09:28:08 -0400

    todo

--- js/scriptsvideos.js
+++ js/scriptsvideos.js
@@ -19,32 +19,35 @@
 
 
 
+    function openModal(modalId) {
+        var modal = document.getElementById(modalId);
+        modal.style.display = "block";
 
-
-
-function openModal(modalId) {
-    var modal = document.getElementById(modalId);
-    modal.style.display = "block";
-}
-
-function closeModal(modalId) {
-    var modal = document.getElementById(modalId);
-    modal.style.display = "none";
-
-    // Detener el video
-    var video = modal.querySelector('video'); // Selecciona el video dentro del modal
-    if (video) {
-        video.pause(); // Pausa el video
-        video.currentTime = 0; // Reinicia el video al inicio
-    }
-}
-
-// Cerrar el modal si se hace clic fuera de él
-window.onclick = function(event) {
-    var modals = document.getElementsByClassName("modal2");
-    for (var i = 0; i < modals.length; i++) {
-        if (event.target == modals[i]) {
-            closeModal(modals[i].id); // Cierra el modal al hacer clic fuera
+        // Reproducir el video
+        var video = modal.querySelector('video'); // Selecciona el video dentro del modal
+        if (video) {
+            video.play(); // Inicia la reproducción del video
         }
     }
-}
+
+    function closeModal(modalId) {
+        var modal = document.getElementById(modalId);
+        modal.style.display = "none";
+
+        // Detener el video
+        var video = modal.querySelector('video'); // Selecciona el video dentro del modal
+        if (video) {
+            video.pause(); // Pausa el video
+            video.currentTime = 0; // Reinicia el video al inicio
+        }
+    }
+
+    // Cerrar el modal si se hace clic fuera de él
+    window.onclick = function(event) {
+        var modals = document.getElementsByClassName("modal2");
+        for (var i = 0; i < modals.length; i++) {
+            if (event.target == modals[i]) {
+                closeModal(modals[i].id); // Cierra el modal al hacer clic fuera
+            }
+        }
+    }
